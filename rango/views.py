57803915475c7fa843be23fc50c41a7c23ba9dcc
Martin Allen <martyall@Martins-MacBook-Air.local>
2015-04-26 09:15:24 +0200

    starting to refactor registration, about to do change psswd

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 from rango.models import Category, Page
@@ -11,10 +13,40 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
-    category_list_views = Category.objects.order_by('-views')[:5]
-    context_dict = {'categories':category_list,'categories_views':category_list_views}
-    return render(request, 'rango/index.html', context_dict)
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
+
 
 def about(request):
     context_dict = {'boldmessage': "This is the about page!"}
@@ -106,6 +138,9 @@ def add_page(request, category_name_slug):
 
 def register(request):
 
+    #if request.session.test_cookie_worked():
+    #   print ">>> TEST COOKIE WORKED!"
+    #    request.session.delete_test_cookie()
     # A boolean value for telling whethert the reg. was successful
     # Set to false initially. Change to true upon completion.
     registered = False
@@ -185,5 +220,5 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're not logged in, you can see this text")
+    return render(request, 'rango/restricted.html', {})
 
--- tango_with_django_project/settings.py~
+++ tango_with_django_project/settings.py~
@@ -122,3 +122,5 @@ TEMPLATE_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/rango/login/'
